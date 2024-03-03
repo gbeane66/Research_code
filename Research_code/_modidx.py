92414546d85ee55da1845038daadf2cefcbc6c80
Gary Beane <gbeane66@gmail.com>
2024-03-03 23:52:14 +0000

    Added exp_decay

--- Research_code/_modidx.py
+++ Research_code/_modidx.py
@@ -5,4 +5,8 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://gbeane66.github.io',
                 'git_url': 'https://github.com/gbeane66/Research_code',
                 'lib_path': 'Research_code'},
-  'syms': {'Research_code.core': {'Research_code.core.foo': ('core.html#foo', 'Research_code/core.py')}}}
+  'syms': { 'Research_code.core': {'Research_code.core.foo': ('research_code1.html#foo', 'Research_code/core.py')},
+            'Research_code.research_code': { 'Research_code.research_code.exp_decay': ( 'research_code.html#exp_decay',
+                                                                                        'Research_code/research_code.py'),
+                                             'Research_code.research_code.foo': ( 'research_code.html#foo',
+                                                                                  'Research_code/research_code.py')}}}
