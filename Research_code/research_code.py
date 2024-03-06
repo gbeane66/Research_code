9b08db61d3b7ae85e01bf4528dbade73a477d44a
Gary Beane <gbeane66@gmail.com>
2024-03-06 01:23:03 +0000

    Altered the function definition

--- Research_code/research_code.py
+++ Research_code/research_code.py
@@ -6,13 +6,13 @@ __all__ = ['exp_decay', 'foo']
 # %% ../nbs/research_code.ipynb 3
 import numpy as np
 
-def exp_decay(time:np.ndarray, # Input array that contains the time.
-    popt:tuple, # Tuple containing the input parameters.
+def exp_decay(x:np.ndarray, # Input array that contains the indpendent variable.
+    Amplitude: float|int, # Prefactor of the exponential decay.
+    tau: float|int, # Decay constant.
+    x0: float|int, # Offset for the indpendent variable.
     ) -> np.ndarray: # A new float array containing the exponentially decaying function.
 
-    A,tau1,t0 = popt
-
-    decayOutput = A * np.exp(-(time-t0)/tau1)
+    decayOutput = A * np.exp(-(x-x0)/tau1)
     
     return decayOutput
 
